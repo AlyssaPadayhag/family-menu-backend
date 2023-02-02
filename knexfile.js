8debba37ccb6fd4bc2b4046572bf7e5238532cb6
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-02-02 14:18:39 -0500

    fix db

--- knexfile.js
+++ knexfile.js
@@ -8,18 +8,20 @@ require('dotenv').config();
 const path = require("path");
 
 const { 
-  DATABASE_URL  = 'postgresql://postgres@localhost/postgres' 
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
   development: {
-    client: 'postgresql',
+    client: "postgresql",
+    pool: { min: 1, max: 5 },
     connection: DATABASE_URL,
     migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations'),
+      directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-      directory: path.join(__dirname, 'src', 'db', 'seeds'),
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
+    debug: !!DEBUG,
   },
 };
--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 
-const app = express();
 const mealsRouter = require('./meals/meals.router');
+
+const app = express();
 
 app.use(express.json());
 
